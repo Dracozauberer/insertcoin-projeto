f191fbea287f57a1eb68eb38a175432f491a5fb5
Dracozauberer <vsbarcellos20@gmail.com>
2025-11-03 15:47:38 -0300

    chore: preparando para atualizar Angular

--- insertcoin_frontend/src/app/app.routes.ts
+++ insertcoin_frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { VitrineComponent } from './vitrine/vitrine.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/vitrine', pathMatch: 'full' },
+  { path: 'vitrine', component: VitrineComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'cadastro', component: CadastroComponent }
+];
