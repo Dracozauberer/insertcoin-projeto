ec2aca5f060a5a8a7878216099684da2928903ef
Yasmin Feliciano <713yasminlima@gmail.com>
2025-11-20 22:12:21 -0300

    Arrumado o login e o cadastro e modificado o contato

---
+++ insertcoin_frontend/src/app/recsenha/recsenha.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Recsenha } from './recsenha';
+
+describe('Recsenha', () => {
+  let component: Recsenha;
+  let fixture: ComponentFixture<Recsenha>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Recsenha]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Recsenha);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ insertcoin_frontend/src/app/recsenha/recsenha.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recsenha',
+  imports: [],
+  templateUrl: './recsenha.html',
+  styleUrl: './recsenha.css',
+})
+export class Recsenha {
+
+}
